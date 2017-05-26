4c68974d81d433f33856ae4ccb5c2a0ae821bec7
Emma Barker <emma.barker@skybettingandgaming.com>
2017-05-26 16:41:11 +0100

    Adding lightbox to gallery page

--- scripts.js
+++ scripts.js
@@ -7,3 +7,31 @@ $(".intro button").click(function(){
 	$(this).prev().show()
 	$(this).remove();
 });
+
+
+
+var $overlay = $("<div id='overlay'></div>");
+var $image = $("<img>");
+var $caption = $("<p></p>")
+
+$overlay.append($image);
+$overlay.append($caption);
+
+$("#imageGallery").append($overlay);
+
+$("#imageGallery a").click(function(event){
+	event.preventDefault();
+	var imageLocation = $(this).attr("href");
+	$image.attr("src", imageLocation);
+	$overlay.show();
+
+	var captionText = $(this).children("img").attr("alt");
+	$caption.text(captionText);
+});
+
+$overlay.click(function(){
+	$(this).hide();
+
+});
+
+
